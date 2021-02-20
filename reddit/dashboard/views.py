570c73dfa36664f4254932b83e4f14fb1578a5b6
rowenhama <tafadzwarhama@gmail.com>
2021-02-20 20:25:12 +0200

    Create views.py

---
+++ reddit/dashboard/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def dash(request):
+	
+	#return HttpResponse('<h1>hajajja</h1>')
+	return render(request,'index.html')
+
+def about(request):
+	return render(request,'about.html')
+
+def posts(request):
+	return render(request,'posts.html')
+
+
