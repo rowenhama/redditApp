599fb78061294033a7e8f5de9a086e768dbb6c47
rowenhama <tafadzwarhama@gmail.com>
2021-02-23 16:15:13 +0200

    Update viewsets.py

--- reddit/employee/viewsets.py
+++ reddit/employee/viewsets.py
@@ -3,5 +3,5 @@ from . import models
 from . import serializers
 
 class EmployeeViewset(viewsets.ModelViewSet):
-	query = models.Employee.objects.all()
+	queryset = models.Employee.objects.all()
 	serializer_class = serializers.EmployeeSerializer
