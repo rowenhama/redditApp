e915dc671749c324c1568121eb669499f52188ad
rowenhama <tafadzwarhama@gmail.com>
2021-02-25 14:33:15 +0200

    Update views.py

--- reddit/dashboard/views.py
+++ reddit/dashboard/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
 
 def dash(request):
 	
